fe7e4751416f713b13c51ba79ff13dfd941ea688
Eric (Ashley Laptop) <eric.drake2011@gmail.com>
2022-11-26 09:20:45 -0600

    Sent planet info from chosen planet to addDestinationInfo()

--- script.js
+++ script.js
@@ -21,7 +21,15 @@ window.addEventListener("load", function () {
     .then(function () {
       console.log(listedPlanets);
       // Below this comment call the appropriate helper functions to pick a planet fom the list of planets and add that information to your destination.
-
-      console.log(pickPlanet(listedPlanets));
+      let destination = pickPlanet(listedPlanets);
+      addDestinationInfo(
+        document,
+        destination.name,
+        destination.diameter,
+        destination.star,
+        destination.distance,
+        destination.moon,
+        destination.imageUrl
+      );
     });
 });
